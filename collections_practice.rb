b8d3341582c5892ae9c4dbee66712595ebe57b59
jonathan choi <jisung594@gmail.com>
2018-12-06 03:03:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,85 @@
-# your code goes here
+require "pry"
+
+def begins_with_r(tools)
+  tools.each do |ele|
+    if !ele.start_with?("r")
+      return false
+    end
+  end
+  
+  return true
+end
+
+
+def contain_a(arr)
+  a_arr = []
+  
+  arr.each do |ele|
+    chars = ele.split("")
+    if chars.include?("a")
+      a_arr << ele
+    end
+  end
+  
+  return a_arr
+end
+
+
+def first_wa(arr)
+  wa_arr = []
+  
+  arr.each do |ele|
+    str = ele.to_s
+    if str.start_with?("wa")
+      wa_arr << ele
+    end
+  end
+  
+  return wa_arr[0]
+end  
+
+
+def remove_non_strings(arr)
+  arr.delete_if {|ele| ele.class != String}
+  
+  return arr
+end
+
+
+def count_elements(arr)
+  count_arr = []
+  count = {:count => 0}
+  
+  arr.each do |hash|
+    hash.each do |key,val|
+      count[:name] = val
+      if val == val
+        count[:count] += 1
+      binding.pry
+      end
+    end
+  end
+  count_arr << count
+  
+  return count_arr
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
